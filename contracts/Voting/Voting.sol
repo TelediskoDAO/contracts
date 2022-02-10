9c11d34cb93083769c54d0af817146d329b7c55f
Nicola Miotto <miotto@posteo.de>
2022-02-10 16:47:36 +0100

    remove unneded code

--- contracts/Voting/Voting.sol
+++ contracts/Voting/Voting.sol
@@ -1,18 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "../ShareholderRegistry/IShareholderRegistry.sol";
-import "./IVoting.sol";
-
 contract Voting {
-    bytes32 public MANAGER_ROLE = keccak256("MANAGER_ROLE");
-    bytes32 public RESOLUTION_ROLE = keccak256("RESOLUTION_ROLE");
-
-    IShareholderRegistry _shareholderRegistry;
-    IERC20 _token;
-
     bytes32 private _contributorRole;
 
     // TODO Turn into struct
@@ -36,72 +25,7 @@ contract Voting {
         uint256 newVotingPower
     );
 
-    constructor() {
-        //_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-    }
-
-    //modifier onlyToken() {
-    //    require(
-    //        msg.sender == address(_token),
-    //        "Voting: only Token contract can call this method."
-    //    );
-    //    _;
-    // }
-
-    //function canVote(address account) public view returns (bool) {
-    //    return getDelegate(account) != address(0);
-    // }
-
-    //function setToken(IERC20 token) external onlyRole(MANAGER_ROLE) {
-    //    _token = token;
-    //}
-
-    //function setShareholderRegistry(IShareholderRegistry shareholderRegistry)
-    //    external
-    //    onlyRole(MANAGER_ROLE)
-    //{
-    //_shareholderRegistry = shareholderRegistry;
-    //_contributorRole = _shareholderRegistry.CONTRIBUTOR_STATUS();
-    //}
-
-    /*function afterRemoveContributor(address account)
-        external
-        onlyRole(RESOLUTION_ROLE)
-    {
-        address delegated = getDelegate(account);
-        if (delegated != address(0)) {
-            if (delegated != account) {
-                _delegate(account, account);
-            }
-            else {
-                _beforeDelegate(account);
-            }
-            
-            delete _delegates[account];
-
-            uint256 individualVotingPower = _token.balanceOf(account);
-            if (individualVotingPower > 0) {
-                _moveVotingPower(account, address(0), individualVotingPower);
-            }
-        }
-    }*/
-
-    /// @dev Hook to be called by the companion token upon token transfer
-    /// @notice This method is called every time a token is transferred to an account. The amount must have been trasnferred before from another account
-    /// or transferred for the first time by address 0. Therefore, there can never be the case that someone with voting power 0 is transferring
-    /// a token, because that token must have been first trasferred to them, hence increasing their voting power.
-    /// @notice Only the companion token can call this method
-    /// @notice The voting power transfer logic relies on the correct usage of this hook from the companion token
-    /// @param from The sender's address
-    /// @param to The receiver's address
-    /// @param amount The amount sent
-    //function afterTokenTransfer(
-    //    address from,
-    //    address to,
-    //    uint256 amount
-    //) external {
-    //    _moveVotingPower(getDelegate(from), getDelegate(to), amount);
-    // }
+    constructor() {}
 
     /// @dev Returns the account's current delegate
     /// @param account The account whose delegate is requested
@@ -195,46 +119,4 @@ contract Voting {
 
         assert(newDelegate == _delegates[newDelegate]);
     }
-
-    //function _moveVotingPower(
-    //    address fromDelegate,
-    //    address toDelegate,
-    //    uint256 amount
-    //) private {
-    //    if (fromDelegate != toDelegate && amount > 0) {
-    //        if (fromDelegate != address(0)) {
-    //           // _beforeMoveVotingPower(fromDelegate);
-    //            uint256 oldVotingPower = _votingPower[fromDelegate];
-    //            _votingPower[fromDelegate] = oldVotingPower - amount;
-    //            //emit DelegateVotesChanged(
-    //            //    fromDelegate,
-    //            //    oldVotingPower,
-    //            //    _votingPower[fromDelegate]
-    //            //);
-    //        } else {
-    //            //_beforeUpdateTotalVotingPower();
-    //            _totalVotingPower += amount;
-    //        }
-
-    //        if (toDelegate != address(0)) {
-    //            //_beforeMoveVotingPower(toDelegate);
-    //            uint256 oldVotingPower = _votingPower[toDelegate];
-    //            _votingPower[toDelegate] = oldVotingPower + amount;
-    //emit DelegateVotesChanged(
-    //    toDelegate,
-    //    oldVotingPower,
-    //   _votingPower[toDelegate]
-    //);
-    //        } else {
-    //_beforeUpdateTotalVotingPower();
-    //           _totalVotingPower -= amount;
-    //       }
-    //  }
-    // }
-
-    //function _beforeDelegate(address delegator) internal virtual {}
-
-    //function _beforeMoveVotingPower(address account) internal virtual {}
-
-    //function _beforeUpdateTotalVotingPower() internal virtual {}
 }
